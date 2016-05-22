28fb824ea28fc1192f55143526edd819f6b73bab
Yondy Kang <yokangy@gmail.com>
2016-05-22 15:17:19 -0700

    Created an action creator to handle user signup.

--- src/actions/index.js
+++ src/actions/index.js
@@ -33,6 +33,18 @@ export function signinUser({ email, password }) {
   }
 }
 
+export function signupUser({ email, password }) {
+  return function(dispatch) {
+    axios.post(`${ROOT_URL}/signup`, { email, password })
+      .then(response => {
+        dispatch({ type: AUTH_USER });
+        localStorage.setItem('token', response.data.token);
+        browserHistory.push('/feature');
+      })
+      .catch(response => dispatch(authError(response.data.error)));
+  }
+}
+
 export function authError(error) {
   return {
     type: AUTH_ERROR,
