8dc9b2acceedb0af7f4a8b5536de2a0bb7e03e02
Yondy Kang <yokangy@gmail.com>
2016-05-22 15:18:17 -0700

    Added a submit handler for the signup form and display any signup error messages.

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,10 +3,25 @@ import { reduxForm } from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
+  handleFormSubmit(formProps) {
+    // Call action creator to sign up the user
+    this.props.signupUser(formProps);
+  }
+
+  renderAlert() {
+    if(this.props.errorMessage) {
+      return (
+        <div className="alert alert-danger">
+          <strong>Oops!</strong> {this.props.errorMessage}
+        </div>
+      );
+    }
+  }
+
   render() {
     const { handleSubmit, fields: {email, password, passwordConfirm} } = this.props;
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
@@ -22,6 +37,7 @@ class Signup extends Component {
           <input className="form-control" type="password" {...passwordConfirm} />
           {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
+        {this.renderAlert()}
         <button action="submit" className="btn btn-primary">Sign up</button>
       </form>
     );
@@ -50,8 +66,12 @@ function validate(formProps) {
   return errors;
 }
 
+function mapStateToProps(state) {
+  return { errorMessage: state.auth.error };
+}
+
 export default reduxForm({
   form: 'signup',
   fields: ['email', 'password', 'passwordConfirm'],
   validate: validate
-})(Signup);
+}, mapStateToProps, actions)(Signup);
