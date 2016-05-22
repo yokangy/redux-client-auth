4df7bab3f435d6012df7384a6b85dfbc35a38c0c
Yondy Kang <yokangy@gmail.com>
2016-05-21 17:26:06 -0700

    Save JWT in local storage.

--- src/actions/index.js
+++ src/actions/index.js
@@ -13,6 +13,7 @@ export function signinUser({ email, password }) {
         // Update state to indicate user is authenticated
         dispatch({ type: AUTH_USER});
         // Save JWT
+        localStorage.setItem('token', response.data.token);
         // redirect to route '/feature'
         browserHistory.push('/feature');
       })
