f1cc8fef00d909ba4f88da2916486198c297d894
Yondy Kang <yokangy@gmail.com>
2016-05-21 18:01:50 -0700

    Header dynamically displays links based on signed in and signed out states.

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,43 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
+  renderLinks() {
+    if(this.props.authenticated) {
+      return (
+        <li className="nav-item">
+          <Link to="/signout" className="nav-link">Sign Out</Link>
+        </li>
+      );
+    } else {
+      return [
+        <li className="nav-item" key={1}>
+          <Link to="/signin" className="nav-link">Sign In</Link>
+        </li>,
+        <li className="nav-item" key={2}>
+          <Link to="/signup" className="nav-link">Sign Up</Link>
+        </li>
+      ]
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Redux Auth</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">
-            Sign in
-          </li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
 
-export default Header;
+function mapStateToProps(state) {
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
+
+export default connect(mapStateToProps)(Header);
