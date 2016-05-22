36ea02d68ea46cfb619fd46bab60e7bf6d07d82f
Yondy Kang <yokangy@gmail.com>
2016-05-22 13:52:29 -0700

    Added form validation to ensure passwords match on the signup form.

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -4,12 +4,40 @@ import * as actions from '../../actions';
 
 class Signup extends Component {
   render() {
+    const { handleSubmit, fields: {email, password, passwordConfirm} } = this.props;
     return (
-      <div>
-        Signup
-      </div>
+      <form>
+        <fieldset className="form-group">
+          <label>Email:</label>
+          <input className="form-control" {...email} />
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Password:</label>
+          <input className="form-control" type="password" {...password} />
+          {password.touched && passwordConfirm.touched && password.error && <div className="error">{password.error}</div>}
+        </fieldset>
+        <fieldset className="form-group">
+          <label>Confirm Password:</label>
+          <input className="form-control" type="password" {...passwordConfirm} />
+        </fieldset>
+        <button action="submit" className="btn btn-primary">Sign up</button>
+      </form>
     );
   }
 }
 
-export default Signup;
+function validate(formProps) {
+  const errors = {};
+
+  if(formProps.password !== formProps.passwordConfirm) {
+    errors.password = 'Passwords must match';
+  }
+
+  return errors;
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate: validate
+})(Signup);
