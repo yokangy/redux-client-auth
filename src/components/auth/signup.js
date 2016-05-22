7ddf2c263c44a41cf83c32641821ccfb485da237
Yondy Kang <yokangy@gmail.com>
2016-05-22 14:00:59 -0700

    Added form validations to check for existence of data.

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -10,6 +10,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Email:</label>
           <input className="form-control" {...email} />
+          {email.touched && email.error && <div className="error">{email.error}</div>}
         </fieldset>
         <fieldset className="form-group">
           <label>Password:</label>
@@ -19,6 +20,7 @@ class Signup extends Component {
         <fieldset className="form-group">
           <label>Confirm Password:</label>
           <input className="form-control" type="password" {...passwordConfirm} />
+          {passwordConfirm.touched && passwordConfirm.error && <div className="error">{passwordConfirm.error}</div>}
         </fieldset>
         <button action="submit" className="btn btn-primary">Sign up</button>
       </form>
@@ -28,6 +30,18 @@ class Signup extends Component {
 
 function validate(formProps) {
   const errors = {};
+
+  if(!formProps.email) {
+    errors.email = 'Please enter an email';
+  }
+
+  if(!formProps.password) {
+    errors.password = 'Please enter a password';
+  }
+
+  if(!formProps.passwordConfirm) {
+    errors.passwordConfirm = 'Please enter a password confirmation';
+  }
 
   if(formProps.password !== formProps.passwordConfirm) {
     errors.password = 'Passwords must match';
