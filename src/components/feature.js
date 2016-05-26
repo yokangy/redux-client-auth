54497d5a14c08329cea5ca49aab84c411804c658
Yondy Kang <yokangy@gmail.com>
2016-05-25 23:59:46 -0700

    Modified feature page to display message retrieved from server.

--- src/components/feature.js
+++ src/components/feature.js
@@ -6,12 +6,18 @@ class Feature extends Component {
   componentWillMount() {
     this.props.fetchMessage();
   }
-  
+
   render() {
     return (
-      <div>This is a feature</div>
+      <div>
+        {this.props.message}
+      </div>
     );
   }
 }
 
-export default connect(null, actions)(Feature);
+function mapStateToProps(state) {
+  return { message: state.auth.message };
+}
+
+export default connect(mapStateToProps, actions)(Feature);
